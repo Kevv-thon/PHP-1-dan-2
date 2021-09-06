4dcec03012f033350cd54b47470c299c6f039d73
Kevv-thon <kevinbramastaa@gmail.com>
2021-09-06 10:54:18 +0700

    first commit

---
+++ Modul1/latihan5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="variable_hasil.php" method="post">
+        Nama : <input type="text" name="nama" size="50"><br>
+        Email : <input type="text" name="email" size="50"><br>
+        <input type="submit" name="submit" value="Tampilkan">
+    </form>
+</body>
+</html>
---
+++ Modul2/latihan7.php
@@ -0,0 +1,14 @@
+<?php
+
+$arrbuah = array("Mangga","Apel","Pisang","Jeruk");
+echo $arrbuah[0]."<br>"; //Mangga 
+echo $arrbuah[3]."<br>"; //Jeruk
+
+$arrwarna = array();
+$arrwarna[] = "Merah";
+$arrwarna[] = "Biru";
+$arrwarna[] = "Hijau";
+$arrwarna[] = "Putih";
+echo $arrwarna[0]."<br>"; //Merah
+echo $arrwarna[2]."<br>"; //Hijau
+?>
